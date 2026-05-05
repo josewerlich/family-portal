5f4e02ba1d1b2794c5e4c5956dcda2b41af0b0bd
Ed Werlich <ed@familyfinances.uk>
2026-05-05 22:31:22 +0000

    Fix CORS preflight OPTIONS response

--- worker/index.js
+++ worker/index.js
@@ -44,7 +44,17 @@ async function ensureUser(db, email) {
 
 export default {
   async fetch(request, env) {
-    if (request.method === 'OPTIONS') return new Response(null, {headers:CORS});
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Max-Age': '86400',
+        }
+      });
+    }
 
     const url = new URL(request.url);
     const path = url.pathname;
